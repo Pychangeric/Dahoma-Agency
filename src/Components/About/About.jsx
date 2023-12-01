63d75e7f9c5152e48ffcf3dae006195ef3a1a1f7
Nkathaglow <kinyua6945@gmail.com>
2023-12-01 11:58:33 +0300

    :sparkles: Working on styling

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../home/components/Navbar'
 import Numbers from './Acomponent/Numbers'
+import Team from './Acomponent/Team'
 
 function About() {
   return (
     <div>
         <Navbar />
         <Numbers />
+        <Team />
 
     </div>
   )
