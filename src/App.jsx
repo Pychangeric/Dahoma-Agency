2e08f0717bdaca30c07ca31804fefa7b04452d43
Nkathaglow <kinyua6945@gmail.com>
2023-12-01 10:14:24 +0300

    :sparkles: Added the files

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainHome from './Components/home/Main.Home'
+import About from './Components/About/About';
 
 
 function App() {
@@ -14,6 +15,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<MainHome />} />
+          <Route path="/" element={<About />} />
+
         </Routes>
       </Router>
     </>
---
+++ src/Components/About/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../home/components/Navbar'
+import Numbers from './Acomponent/Numbers'
+
+function About() {
+  return (
+    <div>
+        <Navbar />
+        <Numbers />
+
+    </div>
+  )
+}
+
+export default About
