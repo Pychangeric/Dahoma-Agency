8de1f9ab7f65a47745e56ecacb32da74846f3ab2
Nkathaglow <kinyua6945@gmail.com>
2023-12-06 11:48:46 +0300

    Styling

--- src/Components/About/Acomponent/Start.jsx
+++ src/Components/About/Acomponent/Start.jsx
@@ -54,6 +54,217 @@ function Start() {
     </p>
   </div>
 </div>;
+
+<div className="w-[1920px] h-[819px]">
+  <div className="w-[1920px] h-[819px] absolute left-[-0.5px] top-[3835.5px] bg-[#dceaf5]" />
+  <div className="w-[400px] h-[507px]">
+    <div className="w-[400px] h-[277px] absolute left-[1199.5px] top-[4281.5px] rounded-bl-[20px] rounded-br-[20px] bg-white" />
+    <div className="w-[336px] h-[213px]">
+      <div className="w-[120px] h-[17px]">
+        <p className="absolute left-[1232px] top-[4510px] text-base font-medium text-left text-[#f58a07]">
+          Learn More
+        </p>
+        <svg
+          width={22}
+          height={10}
+          viewBox="0 0 22 10"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute left-[1328.5px] top-[4512.5px]"
+          preserveAspectRatio="xMidYMid meet"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M17.6338 0.198869L21.8079 4.51989C22.064 4.78505 22.064 5.21495 21.8079 5.48011L17.6338 9.80113C17.3776 10.0663 16.9623 10.0663 16.7062 9.80113C16.45 9.53597 16.45 9.10606 16.7062 8.8409L19.7606 5.67898L0 5.67898L0 4.32102L19.7606 4.32102L16.7062 1.1591C16.45 0.893936 16.45 0.464029 16.7062 0.198869C16.9623 -0.0662898 17.3776 -0.0662898 17.6338 0.198869Z"
+            fill="#F58A07"
+          />
+        </svg>
+      </div>
+      <p className="w-[336px] absolute left-[1232px] top-[4402px] text-base font-medium text-left text-[#5b5b5b]">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet eros blandit,
+        hendrerit elit et.
+      </p>
+      <p className="w-[336px] absolute left-[1232px] top-[4314px] text-2xl font-medium text-left text-[#0d1317]">
+        Helping the next generation of leaders
+      </p>
+    </div>
+    <img
+      src="image.png"
+      className="w-[400px] h-[230px] absolute left-[1199.5px] top-[4051.5px] rounded-tl-[20px] rounded-tr-[20px] object-cover"
+    />
+  </div>
+  <div className="w-[400px] h-[507px]">
+    <div className="w-[400px] h-[277px] absolute left-[759.5px] top-[4281.5px] rounded-bl-[20px] rounded-br-[20px] bg-white" />
+    <div className="w-[336px] h-[213px]">
+      <div className="w-[120px] h-[17px]">
+        <p className="absolute left-[792px] top-[4510px] text-base font-medium text-left text-[#f58a07]">
+          Learn More
+        </p>
+        <svg
+          width={22}
+          height={10}
+          viewBox="0 0 22 10"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute left-[888.5px] top-[4512.5px]"
+          preserveAspectRatio="xMidYMid meet"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M17.6338 0.198869L21.8079 4.51989C22.064 4.78505 22.064 5.21495 21.8079 5.48011L17.6338 9.80113C17.3776 10.0663 16.9623 10.0663 16.7062 9.80113C16.45 9.53597 16.45 9.10606 16.7062 8.8409L19.7606 5.67898L0 5.67898L0 4.32102L19.7606 4.32102L16.7062 1.1591C16.45 0.893936 16.45 0.464029 16.7062 0.198869C16.9623 -0.0662898 17.3776 -0.0662898 17.6338 0.198869Z"
+            fill="#F58A07"
+          />
+        </svg>
+      </div>
+      <p className="w-[336px] absolute left-[792px] top-[4402px] text-base font-medium text-left text-[#5b5b5b]">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet eros blandit,
+        hendrerit elit et.
+      </p>
+      <p className="w-[336px] absolute left-[792px] top-[4314px] text-2xl font-medium text-left text-[#0d1317]">
+        Our internal process and longerm vision
+      </p>
+    </div>
+    <img
+      src="image-3.png"
+      className="w-[400px] h-[230px] absolute left-[759.5px] top-[4051.5px] rounded-tl-[20px] rounded-tr-[20px] object-cover"
+    />
+  </div>
+  <div className="w-[400px] h-[507px]">
+    <div className="w-[400px] h-[277px] absolute left-[319.5px] top-[4281.5px] rounded-bl-[20px] rounded-br-[20px] bg-white" />
+    <div className="w-[336px] h-[213px]">
+      <div className="w-[120px] h-[17px]">
+        <p className="absolute left-[352px] top-[4510px] text-base font-medium text-left text-[#f58a07]">
+          Learn More
+        </p>
+        <svg
+          width={22}
+          height={10}
+          viewBox="0 0 22 10"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute left-[448.5px] top-[4512.5px]"
+          preserveAspectRatio="xMidYMid meet"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M17.6338 0.198869L21.8079 4.51989C22.064 4.78505 22.064 5.21495 21.8079 5.48011L17.6338 9.80113C17.3776 10.0663 16.9623 10.0663 16.7062 9.80113C16.45 9.53597 16.45 9.10606 16.7062 8.8409L19.7606 5.67898L0 5.67898L0 4.32102L19.7606 4.32102L16.7062 1.1591C16.45 0.893936 16.45 0.464029 16.7062 0.198869C16.9623 -0.0662898 17.3776 -0.0662898 17.6338 0.198869Z"
+            fill="#F58A07"
+          />
+        </svg>
+      </div>
+      <p className="w-[336px] absolute left-[352px] top-[4402px] text-base font-medium text-left text-[#5b5b5b]">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet eros blandit,
+        hendrerit elit et.
+      </p>
+      <p className="w-[336px] absolute left-[352px] top-[4314px] text-2xl font-medium text-left text-[#0d1317]">
+        Why you have to digitalize in 2021
+      </p>
+    </div>
+    <img
+      src="image-2.png"
+      className="w-[400px] h-[230px] absolute left-[319.5px] top-[4051.5px] rounded-tl-[20px] rounded-tr-[20px] object-cover"
+    />
+  </div>
+  <p className="w-[453px] absolute left-80 top-[3932px] text-5xl font-semibold text-left capitalize text-[#0d1317]">
+    Latest Blog &amp; News
+  </p>
+</div>;
+
+<footer className="absolute w-[1920px] h-[463px] top-[4654.5px] left-0 bg-transparent">
+                    <div className="relative w-[1920px] h-[463px]">
+                        <div className="absolute w-[1920px] h-[463px] top-0 left-0 bg-[#063155]">
+                            <div className="w-[1282px] top-[359px] left-[320px] absolute h-[24px]">
+                                <div className="w-[302px] top-0 left-[982px] absolute h-[24px]">
+                                    <div className="absolute top-0 left-0 [font-family:'Poppins-Medium',Helvetica] font-medium text-white text-[16px] tracking-[0] leading-[normal]">
+                                        Privacy Policy
+                                    </div>
+                                    <div className="absolute top-0 left-[142px] [font-family:'Poppins-Medium',Helvetica] font-medium text-white text-[16px] tracking-[0] leading-[normal]">
+                                        Terms &amp; Conditions
+                                    </div>
+                                </div>
+                                <p className="absolute top-0 left-0 [font-family:'Poppins-Medium',Helvetica] font-medium text-white text-[16px] tracking-[0] leading-[normal]">
+                                    © All rights reserved – Finsweet
+                                </p>
+                            </div>
+                            <div className="h-[287px] top-[38px] left-[1867px] bg-[#f58a07] rounded-[20px_0px_0px_20px] absolute w-[53px]" />
+                            <img className="h-[144px] top-[319px] left-0 absolute w-[53px]" alt="Shape left" src="../src/assets/Shape Left.svg" />
+                        </div>
+                        <div className="absolute w-[1280px] h-[199px] top-[80px] left-[320px]">
+                            <div className="absolute w-[344px] h-[175px] top-0 left-[938px]">
+                                <button className="absolute w-[158px] h-[54px] top-[121px] left-0 all-[unset] box-border">
+                                    <div className="w-[156px] h-[54px] bg-[#f58a07] relative rounded-[31px]">
+                                        <div className="absolute top-[17px] left-[32px] [font-family:'Poppins-SemiBold',Helvetica] font-semibold text-white text-[18px] tracking-[0] leading-[19.2px] whitespace-nowrap">
+                                            Subscribe
+                                        </div>
+                                    </div>
+                                </button>
+                                <div className="absolute w-[344px] h-[54px] top-[51px] left-0">
+                                    <input
+                                        className="relative w-[342px] h-[54px] bg-white rounded-[31px] border-[none] [font-family:'Inter-Medium',Helvetica] font-medium text-[#9c9c9c] text-[16px] tracking-[0] leading-[normal] whitespace-nowrap p-0 pl-[32px]"
+                                        placeholder="Type email here"
+                                        type="email"
+                                    />
+                                </div>
+                                <div className="absolute top-0 left-0 [font-family:'Poppins-SemiBold',Helvetica] font-semibold text-white text-[18px] tracking-[0] leading-[normal]">
+                                    Join Newsletter
+                                </div>
+                            </div>
+                            <div className="w-[148px] h-[117px] left-[659px] absolute top-0">
+                                <div className="absolute top-[93px] left-0 [font-family:'Poppins-Medium',Helvetica] font-medium text-white text-[16px] tracking-[0] leading-[normal]">
+                                    +(123) 456-7890
+                                </div>
+                                <div className="absolute top-[53px] left-0 [font-family:'Poppins-Medium',Helvetica] font-medium text-white text-[16px] tracking-[0] leading-[normal]">
+                                    hi@finsweet.com
+                                </div>
+                                <div className="left-0 [font-family:'Poppins-SemiBold',Helvetica] font-semibold text-white text-[18px] tracking-[0] leading-[normal] absolute top-0">
+                                    Connect
+                                </div>
+                            </div>
+                            <div className="w-[102px] h-[195px] left-[461px] absolute top-0">
+                                <div className="absolute top-[171px] left-0 [font-family:'Poppins-Medium',Helvetica] font-medium text-white text-[16px] tracking-[0] leading-[normal]">
+                                    Blog
+                                </div>
+                                <div className="absolute top-[131px] left-0 [font-family:'Poppins-Medium',Helvetica] font-medium text-white text-[16px] tracking-[0] leading-[normal]">
+                                    Services
+                                </div>
+                                <div className="absolute top-[91px] left-0 [font-family:'Poppins-Medium',Helvetica] font-medium text-white text-[16px] tracking-[0] leading-[normal]">
+                                    Careers
+                                </div>
+                                <div className="absolute top-[51px] left-0 [font-family:'Poppins-Medium',Helvetica] font-medium text-white text-[16px] tracking-[0] leading-[normal]">
+                                    About Us
+                                </div>
+                                <div className="left-0 [font-family:'Poppins-SemiBold',Helvetica] font-semibold text-white text-[18px] tracking-[0] leading-[normal] absolute top-0">
+                                    Company
+                                </div>
+                            </div>
+                            <div className="absolute w-[353px] h-[199px] top-0 left-0">
+                                <div className="w-[136px] top-[179px] left-0 absolute h-[20px]">
+                                    <img
+                                        className="w-[21px] top-0 left-[115px] absolute h-[20px]"
+                                        alt="Social icon twitter"
+                                        src="../src/assets/twitter.svg"
+                                    />
+                                    <img
+                                        className="w-[21px] top-0 left-0 absolute h-[20px]"
+                                        alt="Social icon facebook"
+                                        src="../src/assets/facebook.svg"
+                                    />
+                                    <img className="w-[21px] top-0 left-[76px] absolute h-[20px]" alt="Social icon" src="../src/assets/instagram.svg" />
+                                    <div className="w-[21px] top-0 left-[38px] absolute h-[20px]">
+                                        <img className="absolute w-[20px] h-[13px] top-[3px] left-px" alt="Subtract" src="../src/assets/youtube.svg" />
+                                    </div>
+                                </div>
+                                <div className="absolute w-[351px] top-[59px] left-0 [font-family:'Poppins-SemiBold',Helvetica] font-semibold text-white text-[36px] tracking-[-1.08px] leading-[44px]">
+                                    Bespoke software solutions
+                                </div>
+                                <img className="w-[120px] h-[27px] top-0 absolute left-0" alt="Logo" src="../src/assets/Logo.svg" />
+                            </div>
+                        </div>
+                    </div>
+                </footer>
     </>
   )
 }
