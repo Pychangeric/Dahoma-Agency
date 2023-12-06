51ededb18c16b7b046369b110789b599ea8929d6
Nkathaglow <kinyua6945@gmail.com>
2023-12-06 14:46:34 +0300

    :sparkles: :art: Designed and styled the component

--- src/Components/home/Main.Home.jsx
+++ src/Components/home/Main.Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Intro from './components/Intro'
 import Detail from './components/Detail'
 import Blog from './components/Blog'
+import Start from './components/Start'
+import Strategies from './components/Strategies'
 import Footer from"./components/Footer"
 import Navbar from './components/Navbar'
 function MainHome() {
@@ -11,6 +13,8 @@ function MainHome() {
             <Intro />
             <Detail />
             <Blog />
+            <Start />
+            <Strategies />
             {/* <Footer /> */}
         </div>
     )
